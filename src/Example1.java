16bb1d9167c146d162517f4a6614d8fd21ebfc25
stealthness <stevewestinlondon@gmail.com>
2026-01-15 18:37:43 +0000

    example 1 code

--- src/Example1.java
+++ src/Example1.java
@@ -1,4 +1,26 @@
+import bank.BankAccount;
+import bank.Storage;
+
 public class Example1 {
     public static void main(String... args) {
+
+        Storage<bank.BankAccount> bankAccountStorage = new Storage<bank.BankAccount>();
+        bank.Storage<String> stringStorage = new bank.Storage<String>();
+
+        // Both storage are constructed successfully, however they are both empty containing null values.
+
+        System.out.println("BankAccount Storage contains: " + bankAccountStorage.getItem());
+        System.out.println("String Storage contains: " + stringStorage.getItem());
+
+        bankAccountStorage.setItem(new BankAccount(1234));
+        stringStorage.setItem("Hello, Generics!");
+
+        System.out.println("BankAccount Storage now contains account number: " + bankAccountStorage.getItem().getAccountNumber());
+        System.out.println("String Storage now contains: " + stringStorage.getItem());
+
+        /* uncompilable code examples
+        Storage<BankAccount> anotherBankAccountStorage = Storage<String>; // Error: incompatible types
+        */
+
     }
 }
